60b77e0c67e5431a4eefd22613e544e6df393694
mperrotta <mperrotta@boic.com>
2011-06-21 11:59:18 -0400

    Windows event log fixes

--- SourceCode/LoggingManagement/B1.LoggingManagement/Constants.cs
+++ SourceCode/LoggingManagement/B1.LoggingManagement/Constants.cs
@@ -12,5 +12,9 @@ namespace B1.LoggingManagement
         internal const string LogFileDirectory = "LogFileDirectory";
         internal const string LogFileNamePrefix = "LogFileNamePrefix";
         internal const string LogFileSize = "LogFileSize";
+
+        //Actual windows event log has max message size of 32766 bytes(not string length). We take away 1000 bytes for 
+        //extra header information we might add such as event reference and continuation msg.
+        internal const int MaxMessageSize = 31766;
     }
 }
